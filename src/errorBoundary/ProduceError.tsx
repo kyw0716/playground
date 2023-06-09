6ceda8641bb2a4cf0944c5f14c56170b7794632d
kyw0716 <kyw0716@naver.com>
2023-06-09 11:41:39 +0900

    style: 요청 후 반환값 보여주는 부분 추가

--- src/errorBoundary/ProduceError.tsx
+++ src/errorBoundary/ProduceError.tsx
@@ -2,16 +2,21 @@ import { useState } from 'react';
 import { useFetch } from '../hooks/useFetch';
 
 export const ProduceErrorWhenClicked = () => {
-  const [아무URL요청] = useFetch('dd');
+  const [unknownRequest] = useFetch('dd');
   const [data, setData] = useState('');
 
   const produceError = async () => {
-    const 결과값 = await 아무URL요청<string>('아무 URL', 'GET');
+    const unknownResult = await unknownRequest<string>('아무 URL', 'GET');
 
-    if (결과값) setData(결과값);
+    setData(unknownResult);
   };
 
-  return <button onClick={produceError}>클릭하면 에러 발생!</button>;
+  return (
+    <>
+      <button onClick={produceError}>클릭하면 에러 발생!</button>
+      <div>{data}</div>
+    </>
+  );
 };
 
 export const ProduceErrorWhenRendered = () => {
