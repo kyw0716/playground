1179cf1aa4aed68e5c05abd525c4f7a42ca22800
kyw0716 <kyw0716@naver.com>
2023-06-21 00:46:47 +0900

    style: console.log 삭제

--- src/hooks/useAsyncErrorBoundary.ts
+++ src/hooks/useAsyncErrorBoundary.ts
@@ -3,8 +3,6 @@ import { useState } from 'react';
 export const useAsyncErrorBoundary = () => {
   const [error, setError] = useState<Error | null>(null);
 
-  console.log(error);
-
   if (error !== null) throw error;
 
   return { throwErrorAtRenderTime: setError };
