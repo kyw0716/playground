bc574b13898ba87ec42d98abf0a61613f9abd6a2
kyw0716 <kyw0716@naver.com>
2023-07-09 14:27:22 +0900

    design: TodoPage 컴포넌트 UI 수정

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,11 +1,17 @@
+import { FlexBox } from '../components/layout/FlexBox';
 import { Layout } from '../components/layout/Layout';
+import { Margin } from '../components/layout/Margin';
 import Todo from '../components/todos';
 
 function TodoPage() {
   return (
     <Layout title="TODO">
-      <Todo />
-      <Todo />
+      <Margin direction="column" size={20} />
+      <FlexBox direction={'column'} alignItems="center">
+        <Todo />
+        <Margin direction="column" size={20} />
+        <Todo />
+      </FlexBox>
     </Layout>
   );
 }
